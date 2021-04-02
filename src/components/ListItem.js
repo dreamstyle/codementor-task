ee9087cfbf0b91f1ed8b734187b72b9d84bdbc6a
Nate Lee <mydreamstyle@gmail.com>
2021-04-03 01:29:51 +0800

    Implement Tag component for category

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Tag from './Tag'
 import Item from './styles/ListItemStyle'
 
 const ListItem = ({ item }) => {
@@ -7,11 +8,7 @@ const ListItem = ({ item }) => {
       <h1 className="title">{item.title}</h1>
       <span className="author">{item.author_name}</span>
       {item.categories.map((category) => {
-        return (
-          <span className="category" key={category}>
-            {category}
-          </span>
-        )
+        return <Tag category={category} key={category} />
       })}
     </Item>
   )
